9abefe6ff2435bdfa9eb8e19442d9f77e84b8c6a
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-02-11 03:56:15 +0100

    Fidia

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <h1 class="text-3xl font-bold bg-slate-200 underline">
+    Hello world!
+  </h1>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
